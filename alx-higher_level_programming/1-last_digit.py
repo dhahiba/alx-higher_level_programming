4f27b8aa5bf8226ab372c8ba555def0e4847f9ed
root <dhhiba1991@gmail.com>
2023-09-05 07:21:43 -0700

    test

---
+++ alx-higher_level_programming/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+    di = number % 10
+else:
+    di = number % -10
+if di == 0:
+    print(f"Last digit of {number:d} is {di:d} and is 0")
+elif di > 5:
+    print(f"Last digit of {number:d} is {di:d} and is greater than 5")
+else:
+    print(f"Last digit of {number:d} is {di:d} and is less than 6 and not 0")
